401a20ecbf1f48edb8ff139e84fe9757b53b017b
OQueLucas <lucasestevam365@gmail.com>
2024-02-05 20:37:41 -0300

    Adicionar arquivos de projeto.

---
+++ VShop.ProductApi/Context/AppDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Security.Cryptography.X509Certificates;
+using VShop.ProductApi.Models;
+
+namespace VShop.ProductApi.Context
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Product> Products { get; set; }
+    }
+}
