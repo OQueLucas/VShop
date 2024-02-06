261a4759a27539e2b550ecfd9dca828f52fafc90
OQueLucas <lucasestevam365@gmail.com>
2024-02-05 22:06:13 -0300

    Implementando DTOs, AutoMapper e Repositories

--- VShop.ProductApi/Context/AppDbContext.cs
+++ VShop.ProductApi/Context/AppDbContext.cs
@@ -1,57 +1,56 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using VShop.ProductApi.Models;
 
-namespace VShop.ProductApi.Context
+namespace VShop.ProductApi.Context;
+
+public class AppDbContext : DbContext
 {
-    public class AppDbContext : DbContext
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
     {
-        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
-        {
-        }
+    }
 
-        public DbSet<Category> Categories { get; set; }
-        public DbSet<Product> Products { get; set; }
+    public DbSet<Category> Categories { get; set; }
+    public DbSet<Product> Products { get; set; }
 
-        // Fluent API
-        protected override void OnModelCreating(ModelBuilder mb)
-        {
-            mb.Entity<Category>().HasKey(c => c.Id);
+    // Fluent API
+    protected override void OnModelCreating(ModelBuilder mb)
+    {
+        mb.Entity<Category>().HasKey(c => c.Id);
 
-            mb.Entity<Category>()
-                .Property(c => c.Name)
-                .HasMaxLength(100)
-                .IsRequired();
+        mb.Entity<Category>()
+            .Property(c => c.Name)
+            .HasMaxLength(100)
+            .IsRequired();
 
-            mb.Entity<Product>()
-                .Property(c => c.Name)
-                .HasMaxLength(100)
-                .IsRequired();
+        mb.Entity<Product>()
+            .Property(c => c.Name)
+            .HasMaxLength(100)
+            .IsRequired();
 
-            mb.Entity<Product>()
-                .Property(c => c.Description)
-                .HasMaxLength(255)
-                .IsRequired();
+        mb.Entity<Product>()
+            .Property(c => c.Description)
+            .HasMaxLength(255)
+            .IsRequired();
 
-            mb.Entity<Product>()
-                .Property(c => c.ImageURL)
-                .HasMaxLength(255)
-                .IsRequired();
+        mb.Entity<Product>()
+            .Property(c => c.ImageURL)
+            .HasMaxLength(255)
+            .IsRequired();
 
-            mb.Entity<Product>()
-                .Property(c => c.Price)
-                .HasPrecision(12, 2);
+        mb.Entity<Product>()
+            .Property(c => c.Price)
+            .HasPrecision(12, 2);
 
-            mb.Entity<Category>()
-                .HasMany(g => g.Products)
-                .WithOne(c => c.Category)
-                .IsRequired()
-                .OnDelete(DeleteBehavior.Cascade);
+        mb.Entity<Category>()
+            .HasMany(g => g.Products)
+            .WithOne(c => c.Category)
+            .IsRequired()
+            .OnDelete(DeleteBehavior.Cascade);
 
 
-            mb.Entity<Category>().HasData(
-                new Category { Id = 1, Name = "Material Escolar" },
-                new Category { Id = 2, Name = "Acessórios" }
-            );
-        }
+        mb.Entity<Category>().HasData(
+            new Category { Id = 1, Name = "Material Escolar" },
+            new Category { Id = 2, Name = "Acessórios" }
+        );
     }
 }
