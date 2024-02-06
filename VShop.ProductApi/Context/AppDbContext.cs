1040cf3839b3cabb826f4352fa1505242caa131d
OQueLucas <lucasestevam365@gmail.com>
2024-02-05 21:11:00 -0300

    Criando migrations e adicionando Fluent API

--- VShop.ProductApi/Context/AppDbContext.cs
+++ VShop.ProductApi/Context/AppDbContext.cs
@@ -1,5 +1,4 @@
 ﻿using Microsoft.EntityFrameworkCore;
-using System.Security.Cryptography.X509Certificates;
 using VShop.ProductApi.Models;
 
 namespace VShop.ProductApi.Context
@@ -12,5 +11,47 @@ namespace VShop.ProductApi.Context
 
         public DbSet<Category> Categories { get; set; }
         public DbSet<Product> Products { get; set; }
+
+        // Fluent API
+        protected override void OnModelCreating(ModelBuilder mb)
+        {
+            mb.Entity<Category>().HasKey(c => c.Id);
+
+            mb.Entity<Category>()
+                .Property(c => c.Name)
+                .HasMaxLength(100)
+                .IsRequired();
+
+            mb.Entity<Product>()
+                .Property(c => c.Name)
+                .HasMaxLength(100)
+                .IsRequired();
+
+            mb.Entity<Product>()
+                .Property(c => c.Description)
+                .HasMaxLength(255)
+                .IsRequired();
+
+            mb.Entity<Product>()
+                .Property(c => c.ImageURL)
+                .HasMaxLength(255)
+                .IsRequired();
+
+            mb.Entity<Product>()
+                .Property(c => c.Price)
+                .HasPrecision(12, 2);
+
+            mb.Entity<Category>()
+                .HasMany(g => g.Products)
+                .WithOne(c => c.Category)
+                .IsRequired()
+                .OnDelete(DeleteBehavior.Cascade);
+
+
+            mb.Entity<Category>().HasData(
+                new Category { Id = 1, Name = "Material Escolar" },
+                new Category { Id = 2, Name = "Acessórios" }
+            );
+        }
     }
 }
