ba610f3d25f6afabfd14ddc85977fd722b72fe06
OQueLucas <lucasestevam365@gmail.com>
2024-02-07 15:41:34 -0300

    Alterando mapeamento e DTO e adicionando o consumo Web para API

---
+++ VShop.Web/Services/Contracts/IProductService.cs
@@ -0,0 +1,13 @@
+﻿using VShop.Web.Models;
+
+namespace VShop.Web.Services.Contracts
+{
+    public interface IProductService
+    {
+        Task<IEnumerable<ProductViewModel>> GetAllProducts();
+        Task<ProductViewModel> FindProductById(int id);
+        Task<ProductViewModel> CreateProduct(ProductViewModel product);
+        Task<ProductViewModel> UpdateProduct(ProductViewModel product);
+        Task<ProductViewModel> DeleteProduct(int id);
+    }
+}
